21ad448fd33fe0e9ff8a431c087a145d30f5f2ce
Dmitry Ulyanov <sillent1987@gmail.com>
2014-01-23 14:51:22 +0400

    Initial Commit

---
+++ RSSreader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RSSreader
+//
+//  Created by Dmitry Ulyanov on 23.01.14.
+//  Copyright (c) 2014 MegaFon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
