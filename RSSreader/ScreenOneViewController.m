abe4e82ddede71527ecbb5d8a6706eaf4040e60d
Dmitry Ulyanov <sillent1987@gmail.com>
2014-08-08 17:13:01 +0400

    удалил ненужные файлы. мелкие корректировки в коде

--- RSSreader/ScreenOneViewController.m
+++ RSSreader/ScreenOneViewController.m
@@ -11,15 +11,14 @@
 
 @interface ScreenOneViewController ()
 {
-//	NSArray *cellArray;
-	__strong NSMutableArray *itemArray;
+
+	NSMutableArray *itemArray;
     Item *item;
     BOOL isLoadRss;
     NSMutableString *itemString;
     NSInteger counterItem;
     
     BaseDumper *bd;
-
 
     BOOL itemBegin;
     BOOL titleBegin;
@@ -106,7 +105,6 @@
     [super viewDidLoad];
 	UIBarButtonItem *barBit=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(updateDataFromButton:)];
     [[self navigationItem]setRightBarButtonItem:barBit animated:YES];
-//	[buttonReload ]
     [[self navigationItem]setTitle:@"Взгляд"];
     loadBegin=NO;
     [self loadContent];
@@ -168,48 +166,6 @@
 {
     return YES;
 }
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a story board-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-
- */
 
 #pragma XMLParsers implements
 -(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict
